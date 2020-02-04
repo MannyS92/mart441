550bd5231dc0d63e1c7eea74b06591764f50ea64
MannyS92 <estewart810@yahoo.com>
2020-02-03 21:46:59 -0600

    Initial Structure of Game

--- hw-2/script.js
+++ hw-2/script.js
@@ -10,3 +10,14 @@ console.log('My 3 Favorite Artists');
 console.log('Keith Haring');
 console.log('Ray Charles');
 console.log('Akira Kurosawa');
+
+var letsPlay = confirm("You wake up and are in a strangeland. In front of you is an old book covered in moss and smells like smoke. Do you open it ?");
+
+if(letsPlay == true) {
+
+   confirm("When you open the book,the world becomes dark again and you are transported into the middle of mideval dungeon. In front of you is a skeleton holding onto a sword. Do you grab the sword ?");
+}
+
+else{
+   alert("Then you go back to your miserable life and never achieve any glory or mead. Hope youre happy! ");
+}
