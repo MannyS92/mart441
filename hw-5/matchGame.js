d15a471fd1e5e67997cebc1d8f1026c79e299e72
Emmanuel Stewart <estewart810@yahoo.com>
2020-02-26 00:20:10 -0600

    Matching Game
    Everything

--- hw-5/matchGame.js
+++ hw-5/matchGame.js
@@ -1,8 +1,20 @@
 var imageTags = ["image1", "image2", "image3", "image4","image5","image6", "image7", "image8", "image9","image10"];
 
 var actualBlank = 'images/sea2.jpeg';
+var firstNumber = -1;
+var secondNumber = -1;
+var attempts = [];
+var setMade = [];
+
+
+
+//JSON player info
+
+var player = {"firstname":"","lastname":"","age":"","attempts":""};
 
 var actualImages = new Array();
+
+
 
 
 function showBlank()
@@ -42,7 +54,85 @@ function showBlank()
 
   }
 
-  function flipImage(number)
+  function flipImage(number) {
+
+
+
+    if (firstNumber >= 0) {
+      secondNumber = number;
+      document.getElementById(imageTags[number]).src = actualImages[secondNumber];
+
+    }
+
+    else if(firstNumber < 0) {
+      firstNumber = number;
+      document.getElementById(imageTags[firstNumber]).src= actualImages[firstNumber];
+    }
+
+    if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+    {
+        setTimeout(imagesDisappear, 1000);
+      attempts.push("attempt");
+      console.log(attempts.length);
+    }
+
+    else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+    {
+        firstNumber = -1;
+        secondNumber = -1;
+        setMade.push(1);
+        console.log(setMade.length);
+  }
+
+  if(setMade.length >= 5){
+    attempts = attempts.length;
+    player.attempts = attempts;
+    localStorage.setItem("playerInfo", JSON.stringify(player));
+
+
+
+    window.location = ("lastpage.html");
+
+  }
+}
+
+
+  function imagesDisappear(){
+    document.getElementById(imageTags[firstNumber]).src= actualBlank;
+    document.getElementById(imageTags[secondNumber]).src= actualBlank;
+    firstNumber = -1;
+    secondNumber = -1;
+  }
+
+
+
+  function fillProfile() {
+    var firstName = document.getElementById("txtFirstName").value;
+    var lastName = document.getElementById("txtLastName").value;
+    var playerAge = document.getElementById("txtAge").value;
+    attempts = attempts.length;
+    player.attempts = attempts;
+
+
+    player.firstname = firstName;
+    player.lastname = lastName;
+    player.age = playerAge;
+
+    localStorage.setItem("playerInfo", JSON.stringify(player));
+    window.location = "matching.html";
+
+  }
+
+
+ function playerInfo()
   {
-      document.getElementById(imageTags[number]).src= actualImages[number];
+    var playerInformation = localStorage.getItem("playerInfo");
+    player = JSON.parse(playerInformation);
+    console.log(player.firstname + player.attempts);
+  }
+
+  function display()
+  {
+
+    document.getElementById("final").innerHTML = ("You did it " + player.firstname + " " + player.lastname + " age " + player.age + ", in " + player.attempts + " attempts");
   }
