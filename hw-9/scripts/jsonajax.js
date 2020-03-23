f85e15bc00d116f25ffe04254c8e2022e835592a
Emmanuel Stewart <estewart810@yahoo.com>
2020-03-23 16:32:03 -0400

    Added MD

--- hw-9/scripts/jsonajax.js
+++ hw-9/scripts/jsonajax.js
@@ -1,25 +1,29 @@
- $(document).ready(function () {
+ $(document).ready(function() {
 
    $.fn.spin = function animateBall() {
-         $("#pokeball").animate({right: "-=200"}, "slow").animate({right: "+=200"}, "slow");
-     };
+     $("#pokeball").animate({
+       right: "-=200"
+     }, "slow").animate({
+       right: "+=200"
+     }, "slow");
+   };
 
-            $("button").click(function () {
-              $.getJSON("data/pokemon.json", function(result){
+   $("button").click(function() {
+     $.getJSON("data/pokemon.json", function(result) {
 
-   $.each(result, function(i, field){
-       var entry = field[Math.floor(Math.random()*field.length)];
-       console.log(entry);
-     $("#pokeHandout").text("Congratualations, you got " + entry.name + " !!!");
-     $("#pokeImage").attr("src", entry.img);
-     $("#pokeBeginText").fadeOut();
-     $("#btnSubmit").text("Unhappy ? Pick another");
-     $("#pokeName").text(entry.name);
-     $("#pokeType").text(entry.type);
-     $("#pokeWeak").text(entry.weaknesses);
-     $("#pokeball").spin();
+       $.each(result, function(i, field) {
+         var entry = field[Math.floor(Math.random() * field.length)];
+         console.log(entry);
+         $("#pokeHandout").text("Congratualations, you got " + entry.name + " !!!");
+         $("#pokeImage").attr("src", entry.img);
+         $("#pokeBeginText").fadeOut();
+         $("#btnSubmit").text("Unhappy ? Pick another");
+         $("#pokeName").text(entry.name);
+         $("#pokeType").text(entry.type);
+         $("#pokeWeak").text(entry.weaknesses);
+         $("#pokeball").spin();
 
+       });
+     });
    });
  });
-});
-});
